20dc75a37df86bd3617ef526f28707c3c2521d99
Michael Porter <mdporter83@gmail.com>
2016-12-07 00:49:47 +0000

    twentyone.rb after rubocop 2 size errors

---
+++ twentyone.rb
@@ -0,0 +1,150 @@
+
+SUITS = %w(Hearts Clubs Spades Diamonds).freeze
+FACES = %w(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King).freeze
+
+def initialize_deck
+  FACES.product(SUITS).shuffle
+end
+
+def clear_screen
+  system('cls') || system('clear')
+end
+
+def hand_total(cards)
+  face_value = cards.map { |card| card[0] }
+
+  total = 0
+
+  face_value.each do |face|
+    total += if face.start_with?("A")
+               11
+             elsif face.to_i.zero?
+               10
+             elsif face.start_with?("1")
+               10
+             else
+               face.to_i
+             end
+  end
+
+  face_value.select { |face| face.start_with?("A") }.count.times do
+    total -= 10 if total > 21
+  end
+
+  total
+end
+
+def bust?(cards)
+  hand_total(cards) > 21
+end
+
+def another_hand?
+  puts "(Y)es to play another hand, or anything else to quit."
+  another_round_answer = gets.chomp.downcase
+  another_round_answer == 'y'
+end
+
+def compare_hands(player_hand, dealer_hand)
+  player_hand_total = hand_total(player_hand)
+  dealer_hand_total = hand_total(dealer_hand)
+
+  if player_hand_total > 21
+    :player_bust
+  elsif dealer_hand_total > 21
+    :dealer_bust
+  elsif player_hand_total > dealer_hand_total
+    :player_wins
+  elsif dealer_hand_total > player_hand_total
+    :dealer_wins
+  else
+    :tie
+  end
+end
+
+def declare_winner(player_hand, dealer_hand)
+  result = compare_hands(player_hand, dealer_hand)
+
+  case result
+  when :player_bust
+    puts "You Busted.Dealer Won"
+  when :dealer_bust
+    puts "Dealer Busted.You Won"
+  when :player_wins
+    puts "You Won"
+  when :dealer_wins
+    puts "Dealer Won"
+  when :tie
+    puts "It's a Tie"
+  end
+end
+
+loop do # main loop
+  deck = initialize_deck.map! { |card| card.join(" of ") }
+  player_hand = []
+  dealer_hand = []
+
+  clear_screen
+  puts 'Welcome to TwentyOne'
+
+  2.times do
+    player_hand << deck.pop
+    dealer_hand << deck.pop
+  end
+
+  puts "Dealer has #{dealer_hand[0]} and an unknown card."
+  puts "You have #{player_hand[0]} and #{player_hand[1]}, totaling " \
+       "#{hand_total(player_hand)}."
+
+  # Player
+
+  loop do
+    player_answer = nil
+
+    loop do
+      puts "Would you like to (h)it or (s)tay?"
+      player_answer = gets.chomp.downcase
+      break if player_answer == 'h' || player_answer == 's'
+      puts "Invalid answer, Would you like to (h)it or (s)tay?"
+    end
+
+    if player_answer == 'h'
+      player_hand << deck.pop
+      puts "Your new hand is #{player_hand}, totaling " \
+           "#{hand_total(player_hand)}."
+    end
+
+    break if player_answer == 's' || bust?(player_hand)
+  end
+
+  if bust?(player_hand)
+    declare_winner(player_hand, dealer_hand)
+    another_hand? ? next : break
+  else
+    puts "You stayed with #{hand_total(player_hand)}"
+  end
+
+  # Dealer
+
+  loop do
+    break if bust?(dealer_hand) || hand_total(dealer_hand) >= 17
+
+    puts "Dealer Hits..."
+    dealer_hand << deck.pop
+    puts "Dealer now has #{dealer_hand} and an unknown card."
+  end
+
+  if bust?(dealer_hand)
+    declare_winner(player_hand, dealer_hand)
+    another_hand? ? next : break
+  else
+    puts "Dealer stays with #{hand_total(dealer_hand)}"
+  end
+
+  puts "~~~~~~~~~~~~~~~~~~"
+  puts "Dealer had #{dealer_hand}, totaling #{hand_total(dealer_hand)}"
+  puts "You had #{player_hand}, totaling #{hand_total(player_hand)}"
+  puts "~~~~~~~~~~~~~~~~~~"
+
+  declare_winner(player_hand, dealer_hand)
+  break unless another_hand?
+end
