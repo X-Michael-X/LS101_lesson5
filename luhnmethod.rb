7a6c6979eceab2993cbeaea69ffb8974e57757a1
Michael Porter <mdporter83@gmail.com>
2016-12-12 18:04:10 +0000

    refactored

--- luhnmethod.rb
+++ luhnmethod.rb
@@ -1,9 +1,11 @@
-def cc_number?(string)
-  double_evens = string.split(' ').join('').split('').map.with_index \
-  { |num, ind| ind.even? ? num.to_i * 2 : num }
+def luhn_cc_number?(string)
+  double_evens = string.split(' ').join('').split('').map.with_index do |n, i|
+    i.even? ? n.to_i * 2 : n
+  end
 
-  check_sum = double_evens.map \
-  { |num| num.to_i > 9 ? num.to_i - 9 : num.to_i }.inject(:+)
+  check_sum = double_evens.map do |num|
+    num.to_i > 9 ? num.to_i - 9 : num.to_i
+  end.inject(:+)
 
   (check_sum % 10).zero?
 end
